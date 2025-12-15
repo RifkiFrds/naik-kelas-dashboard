5405f91ecbe9e2a0f3b61e5f48990cdda4941071
rifkifrds <muhamadrifkifirdaus22@gmail.com>
2025-12-15 21:58:34 +0700

    update: data

--- src/services/eventService.js
+++ src/services/eventService.js
@@ -22,9 +22,11 @@ export const createEvent = async (data) => {
   const form = new FormData();
   form.append("judul", data.judul);
   form.append("deskripsi", data.deskripsi);
+  form.append("type", data.type);
   form.append("tanggal_mulai", fixDate(data.tanggal_mulai));
   form.append("waktu_mulai", formatTime(data.waktu_mulai));
   form.append("status", data.status);
+  form.append("url_cta", data.url_cta);
   form.append("gambar_poster", data.gambar_poster);
 
   return await api.post("/event", form, {
@@ -38,8 +40,10 @@ export const updateEvent = async (id, data) => {
 
   form.append("judul", data.judul);
   form.append("deskprisi", data.deskripsi); 
+  form.append("type", data.type);
   form.append("tanggal_mulai", fixDate(data.tanggal_mulai));
   form.append("waktu_mulai", formatTime(data.waktu_mulai));
+  form.append("url_cta", data.url_cta);
   form.append("status", data.status);
 
   if (data.gambar_poster instanceof File) {
